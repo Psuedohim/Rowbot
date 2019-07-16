6010958331c81d8ae6fc0f07170d1c0bd7bb5bd2
Miguel Agueda <2vlzir5c4xdo@opayq.com>
2019-07-15 18:42:11 -0700

    Wheels self-correct to straight.

--- inoPrograms/toCompile/esp8266_server.ino
+++ inoPrograms/toCompile/esp8266_server.ino
@@ -2,40 +2,47 @@
 #include <WiFiUdp.h>
 #include "src/WiFiCredentials/WiFiCredentials.h"
 
-WiFiCredentials credentials;  // For WiFi credential retrieval.
+// Create instances for WiFi usage.
+// IPAddress ClientIP(192,168,4,2);  // Set client IP.
+// IPAddress ServerIP(192,168,4,1);  // Set server IP.
+WiFiCredentials credentials; // For WiFi credential retrieval.
 WiFiUDP Udp;
-IPAddress ServerIP(192,168,4,1);  // Set server IP.
-IPAddress ClientIP(192,168,4,2);  // Set client IP.
 
-/*
-Variables for WiFi communication and serial communication.
-*/
+// Variables for WiFi communication and serial communication.
 byte incomingByte = 0;
 
-char* SSID = credentials.ssid();  // Retrieve SSID from library.
-char* PASSWORD = credentials.password();  // Retrieve PASSWORD from library.
-char packetBuffer[255];  // Buffer for incoming packets.
+char *SSID = credentials.ssid();         // Retrieve SSID from WiFiCredentials.cpp.
+char *PASSWORD = credentials.password(); // Retrieve PASSWORD from WiFiCredentials.cpp.
+char packetBuffer[255];                  // Buffer for incoming packets.
 
-const int PORT = 80;  // local port to listen on for UDP packets.
-const int BAUD_RATE;  // Baud Rate for serial communication.
+const int PORT = 80;        // Local port to listen on for UDP packets.
+const int BAUD_RATE = 9600; // Baud Rate for serial communication.
 
 // Define pinouts
 #define DriveEn D0
-#define DriveDir D1
-#define DrivePWM D2
-#define SteerEn D3
-#define SteerDir D4
+#define SteerEn D1
+#define DriveDir D2
+#define SteerDir D3
+#define DrivePWM D4
 #define SteerPWM D5
+#define SteerPot A0
 
 // Variables for motor control.
-const int Acceleration = 10;  // Delay for controlling acceleration.
-int DrivePWMValue = 0;  // PWM value for speed control.
+const int DriveAcceleration = 10; // Delay for controlling acceleration.
+const int SteerAcceleration = 5;  // Delay for controlling acceleration.
+int DrivePWMValue = 0;            // PWM value for speed control.
+int SteerPWMValue = 0;            // PWM value for speed control.
+int SteerPotValue = 0;            // potentiometer reading from steering column.
+int MinSteerAngle = 514;          // Lower limit for steering pot value.
+int MidSteerAngle = 882;          // Value when wheels are pointed straight.
+int MaxSteerAngle = 1024;         // Upper limit for steering pot value.
 
 bool forwardsPressed = false;
 bool backwardsPressed = false;
 bool rightPressed = false;
-boot leftPressed = false;
+bool leftPressed = false;
 
+// Byte communication code.
 const int FORWARDS_PRESSED = 1;
 const int FORWARDS_RELEASED = 2;
 const int BACKWARDS_PRESSED = 3;
@@ -45,109 +52,152 @@ const int RIGHT_RELEASED = 6;
 const int LEFT_PRESSED = 7;
 const int LEFT_RELEASED = 8;
 
-
-void initOutputs() {
+void initOutputs()
+{
   // Set motor driver pinouts as output.
-  pinMode(SteerIn1, OUTPUT);
-  pinMode(SteerIn2, OUTPUT);
-  pinMode(SteerIn3, OUTPUT);
-  pinMode(SteerIn4, OUTPUT);
+  pinMode(SteerEn, OUTPUT);
+  pinMode(SteerDir, OUTPUT);
+  pinMode(SteerPWM, OUTPUT);
   pinMode(DriveEn, OUTPUT);
   pinMode(DriveDir, OUTPUT);
   pinMode(DrivePWM, OUTPUT);
 }
 
-
-void setupWiFi() {
-  // Setup access point from ESP8266.
-  // Set SSID, Password from src/WiFiCredentials/WiFiCredentials.h.
-  WiFi.softAP(SSID, PASSWORD); 
-  Udp.begin(PORT);  // Begin UDP on PORT.
+void setupWiFi()
+{
+  WiFi.softAP(SSID, PASSWORD); // Setup access point from ESP8266.
+  Udp.begin(PORT);             // Begin UDP on defined port.
 }
 
-
-void moveForwards() {  //dir high, en high
+void moveForwards()
+{
+  // Serial.println("Forwards");
   digitalWrite(DriveEn, HIGH);
   digitalWrite(DriveDir, HIGH);
   if (DrivePWMValue < 255)
-    DrivePWMValue += 1;
+    DrivePWMValue++;
+
   analogWrite(DrivePWM, DrivePWMValue);
-  delay(Acceleration);
+  delay(DriveAcceleration);
 }
 
-
-void moveBackwards() {  //dir low, en high
+void moveBackwards()
+{
+  // Serial.println("Backwards");
   digitalWrite(DriveEn, HIGH);
   digitalWrite(DriveDir, LOW);
-  if (DrivePWMValue< 255)
-    DrivePWMValue += 1;
+  if (DrivePWMValue < 255)
+    DrivePWMValue++;
+
   analogWrite(DrivePWM, DrivePWMValue);
-  delay(Acceleration);
+  delay(DriveAcceleration);
 }
 
+void turnRight()
+{
+  // Serial.println("Right");
+  if (analogRead(SteerPot) <= MaxSteerAngle) // If wheels not at max:
+  {
+    digitalWrite(SteerEn, HIGH);
+    digitalWrite(SteerDir, HIGH);
+    if (SteerPWMValue < 255)
+      SteerPWMValue++;
 
-void turnRight() {
-
+    analogWrite(SteerPWM, SteerPWMValue);
+    delay(SteerAcceleration);
+  }
 }
 
+void turnLeft()
+{
+  // Serial.println("Left");
+  if (analogRead(SteerPot) >= MinSteerAngle) // If wheels not at min:
+  {
+    digitalWrite(SteerEn, HIGH);
+    digitalWrite(SteerDir, LOW);
+    if (SteerPWMValue < 255)
+      SteerPWMValue++;
 
-void turnLeft() {
-
+    analogWrite(SteerPWM, SteerPWMValue);
+    delay(SteerAcceleration);
+  }
 }
 
+void resetSteering()
+{
+  if (analogRead(SteerPot) < MidSteerAngle)
+    turnRight();
 
-void resetSteering() {
-
+  else if (analogRead(SteerPot) > MidSteerAngle)
+    turnLeft();
 }
 
-
-void resetEngine() {
-
+void resetEngine()
+{
+  digitalWrite(DriveEn, LOW);
+  digitalWrite(DriveDir, LOW);
+  analogWrite(DrivePWM, 0);
+  DrivePWMValue = 0;
 }
 
-
-void detectKeyPresses() {
+void detectKeyPresses()
+{
   // Set truth value corresponding to key pressed.
-  if (incomingByte == FORWARDS_PRESSED) forwardsPressed = true;
-  else if (incomingByte == BACKWARDS_PRESSED) backwardsPressed = true;
+  if (incomingByte == FORWARDS_PRESSED)
+    forwardsPressed = true;
+  else if (incomingByte == BACKWARDS_PRESSED)
+    backwardsPressed = true;
 
-  if (incomingByte == FORWARDS_RELEASED) forwardsPressed = false;
-  else if (incomingByte == BACKWARDS_RELEASED) backwardsPressed = false;
+  if (incomingByte == FORWARDS_RELEASED)
+    forwardsPressed = false;
+  else if (incomingByte == BACKWARDS_RELEASED)
+    backwardsPressed = false;
 
-  if (incomingByte == RIGHT_PRESSED) rightPressed = true;
-  else if (incomingByte == LEFT_PRESSED) leftPressed = true;
+  if (incomingByte == RIGHT_PRESSED)
+    rightPressed = true;
+  else if (incomingByte == LEFT_PRESSED)
+    leftPressed = true;
 
-  if (incomingByte == RIGHT_RELEASED) rightPressed = false;
-  else if (incomingByte == LEFT_RELEASED) leftPressed = false;
+  if (incomingByte == RIGHT_RELEASED)
+    rightPressed = false;
+  else if (incomingByte == LEFT_RELEASED)
+    leftPressed = false;
 }
 
-
-void handlePinOutputs() {
+void handlePinOutputs()
+{
   // Use truth values from detectKeyPresses to control motor driver.
-  if (forwardsPressed) moveForwards();
-  else if (backwardsPressed) moveBackwards();
-  else resetEngine();
+  if (forwardsPressed)
+    moveForwards();
+  else if (backwardsPressed)
+    moveBackwards();
+  else
+    resetEngine();
 
-  if (rightPressed) turnRight();
-  else if (leftPressed) turnLeft();
-  else resetSteering();
+  if (rightPressed)
+    turnRight();
+  else if (leftPressed)
+    turnLeft();
+  else
+    resetSteering();
 }
-
 
 void setup()
 {
-  Serial.begin(9600);  // Begin Serial communication on port 9600.
+  // Serial.begin(BAUD_RATE); // Begin Serial communication on port 9600.
+  initOutputs();
   setupWiFi();
 }
-
 
 void loop()
 {
   int packetSize = Udp.parsePacket();
-  if (packetSize) {  // We've received a UDP packet.
-    // read the packet into the buffer, we are reading only one byte
+  if (packetSize)
+  {
+    // We've received a UDP packet.
     Udp.read(packetBuffer, packetSize);
     incomingByte = packetBuffer[0];
+    // Serial.println(incomingByte);
     delay(20);
   }
   detectKeyPresses();
--- pythonPrograms/esp8266_client.py
+++ pythonPrograms/esp8266_client.py
@@ -7,12 +7,16 @@ class Client:
     def __init__(self, ip, port="80"):  # Default port is 80.
         self._ip = ip
         self._port = port
-    
+        self.pressed = {}
+        self.prev_pressed = {}
+        self._init_presses()
+        self._create_ui()
+
     def _netcat(self, command):
-        sock = socket.(socket.AF_INET, socket.SOCK_DGRAM)  # Udp
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Udp
         sock.sendto(command, (self._ip, self._port))
-    
-    def _initPresses(self):
+
+    def _init_presses(self):
         """Set key press' default state: false."""
         self.pressed["w"] = False
         self.pressed["a"] = False
@@ -26,12 +30,12 @@ class Client:
     def start(self):
         self._check_key_press()
         self.root.mainloop()
-    
+
     def _check_for_press(self, key, command):
         if self._is_pressed(key):
             self.prev_pressed[key] = True
             self._netcat(command)
-        
+
     def _get_byte_for(self, int):
         """Return byte for passed integer."""
         return int.to_bytes(1, byteorder='big')
@@ -42,24 +46,32 @@ class Client:
             self._netcat(command)
 
     def _check_key_press(self):
-        self._check_for_press('w', _get_byte_for(0))
-        self._check_for_release('w', _get_byte_for(1))
-        self._check_for_press('a', _get_byte_for(3))
-        self._check_for_release('a', _get_byte_for(4))
-        self._check_for_press('s', _get_byte_for(5))
-        self._check_for_release('s', _get_byte_for(6))
-        self._check_for_press('d', _get_byte_for(7))
-        self._check_for_release('d', _get_byte_for(8))
+        # self._check_for_press('w', self._get_byte_for(0))
+        # self._check_for_release('w', self._get_byte_for(1))
+        # self._check_for_press('a', self._get_byte_for(3))
+        # self._check_for_release('a', self._get_byte_for(4))
+        # self._check_for_press('s', self._get_byte_for(5))
+        # self._check_for_release('s', self._get_byte_for(6))
+        # self._check_for_press('d', self._get_byte_for(7))
+        # self._check_for_release('d', self._get_byte_for(8))
+        self._check_for_press("w", b"\x01")
+        self._check_for_release("w", b"\x02")
+        self._check_for_press("s", b"\x03")
+        self._check_for_release("s", b"\x04")
+        self._check_for_press("d", b"\x05")
+        self._check_for_release("d", b"\x06")
+        self._check_for_press("a", b"\x07")
+        self._check_for_release("a", b"\x08")
 
         self.root.after(20, self._check_key_press)
 
     def _is_pressed(self, key):
-        return self.pressed[key]
-        self.prev_pressed[key] == False  # Try 'and' if this is buggy.
-    
+        # Try 'and' if this is buggy.
+        return self.pressed[key] and self.prev_pressed[key] == False
+
     def _is_released(self, key):
-        return self.pressed[key] == False
-        self.prev_pressed[key]  # Same as function above.
+        # Same as function above.
+        return self.pressed[key] == False and self.prev_pressed[key]
 
     def _create_ui(self):
         self.root = tk.Tk()
@@ -74,15 +86,14 @@ class Client:
 
     def _pressed(self, event):
         self.pressed[event.char] = True
-    
+
     def _released(self, event):
         self.pressed[event.char] = False
 
-    def run():
 
 # Testing
-ip = "10.0.0.6"
-port = "80"
+ip = "192.168.4.1"
+port = 80
 
 client = Client(ip, port)
 client.start()
