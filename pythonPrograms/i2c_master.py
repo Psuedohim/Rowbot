029cec995ec06fdb09b5e7991c24be26994f9129
Miguel Agueda <2vlzir5c4xdo@opayq.com>
2019-07-22 11:59:36 -0700

    Formatting.

--- pythonPrograms/i2c_master.py
+++ pythonPrograms/i2c_master.py
@@ -2,10 +2,10 @@ import smbus
 import time
 from modules.process_frame import ProcessFrames
 
+MTR_DRV_ADDR = 1
 
-proc = ProcessFrames(0)
-# bus = smbus.SMBus(1)
-motor_addr = 1
+proc = ProcessFrames(4)  # 4 = USB Camera on Rock Pi.
+motor_driver = smbus.SMBus(1)  # Start bus on /ic2-1?
 
 boolToKeepRunning = True
 
@@ -18,4 +18,31 @@ while boolToKeepRunning:
     position = proc.contour_pos(center_x, center_x)
     crosshair_frame = proc.draw_crosshairs(raw_frame, center_x, center_y)
     proc.show_frame(crosshair_frame)
-    
+
+
+def handle_direction(dir_to_go):  # Go forwards.
+    if dir_to_go == 'f':
+        motor_driver.write_byte(MTR_DRV_ADDR, 4)  # Backwards released.
+        motor_driver.write_block(MTR_DRV_ADDR, 1)  # Forwards pressed.
+
+    elif dir_to_go == 'b':  # Go backwards.
+        motor_driver.write_byte(MTR_DRV_ADDR, 2)  # Forwards released.
+        motor_driver.write_byte(MTR_DRV_ADDR, 3)  # Backwards pressed.
+
+    else:  # Stop, brake.
+        motor_driver.write_byte(MTR_DRV_ADDR, 2)  # Forwards released.
+        motor_driver.write_byte(MTR_DRV_ADDR, 4)  # Backwards released.
+
+
+def handle_steering(dir_to_turn):
+    if dir_to_turn == 'r':  # Turn right.
+        motor_driver.write_byte(MTR_DRV_ADDR, 7)  # Left released.
+        motor_driver.write_byte(MTR_DRV_ADDR, 5)  # Right pressed.
+
+    elif dir_to_turn == 'l':  # Turn left.
+        motor_driver.write_byte(MTR_DRV_ADDR, 6)  # Right released.
+        motor_driver.write_byte(MTR_DRV_ADDR, 7)  # Left pressed.
+
+    else:  # Continue straight.
+        motor_driver.write_byte(MTR_DRV_ADDR, 6)  # Right released.
+        motor_driver.write_byte(MTR_DRV_ADDR, 7)  # Left released.
--- pythonPrograms/modules/process_frame.py
+++ pythonPrograms/modules/process_frame.py
@@ -14,9 +14,15 @@ class ProcessFrames:
 
     def update_frame(self):
         """Returns the most recent frame from the camera."""
-        _, frame = self.capture.read()  # Get most recent frame.
-        self.height, self.width, _ = frame.shape  # For use in other functions.
-        return frame
+        if self.capture.isOpened():
+            _, frame = self.capture.read()  # Get most recent frame.
+            # For use in other functions.
+            self.height, self.width, _ = frame.shape
+            return frame
+
+        else:
+            print("Camera not available.\nExiting Program.")
+            exit()
 
     def prep_frame(self, frame_to_prep):
         """Return an frame that is prepared for contour detection."""
@@ -38,7 +44,7 @@ class ProcessFrames:
         contours, _ = cv2.findContours(frame_to_search,
                                        cv2.RETR_TREE,
                                        cv2.CHAIN_APPROX_NONE)
-                                       
+
         # Return sorted list, biggest -> smallest, if list not empty.
         if contours:
             return sorted(contours, key=cv2.contourArea, reverse=True)
@@ -50,7 +56,7 @@ class ProcessFrames:
         """Return x, y coordinates to center of the largest contour in a list."""
         # Isolate and return largest contour from list.
         return max(list_of_contours, key=cv2.contourArea)
-    
+
     def center_coordinates(self, contour):
         """Returns coordinates, x, y, of contour."""
         mnt = cv2.moments(contour)  # Get data from largest contour.
@@ -61,7 +67,7 @@ class ProcessFrames:
         except ZeroDivisionError:
             # If calculating center fails, send null coordinates.
             return -1, -1
-    
+
     def draw_crosshairs(self, frame, x, y):
         """Return frame with crosshair drawn at x, y coordinates."""
         color = (0, 0, 255)
@@ -71,7 +77,7 @@ class ProcessFrames:
         # Draw vertical line on frame.
         cv2.line(frame, (x, 0), (x, self.height), color, thickness)
         return frame
-    
+
     def show_frame(self, frame_to_show):
         cv2.imshow("Current Frame", frame_to_show)
         # cv2.waitKey(0)  # Wait for key press to continue.
