df8989f8c6f759327b87aab246a8ff98594d696c
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-05-26 12:27:54 -0700

    Read/Write I2C, In/Out Structs.

--- rplidar/sdk/app/ultra_simple/i2c_comm.cpp
+++ rplidar/sdk/app/ultra_simple/i2c_comm.cpp
@@ -1,32 +1,51 @@
 #include "i2c_comm.h"
 
+char *filename = (char *)"/dev/i2c-7"; // MCU connected to I2C bus number 7.
+int addr = 0x04;											 // The I2C address of the MCU.
+int file_i2c;
 
-bool write_buffer(int8_t theta, int8_t drive)
+struct I2C_IN_PACK
 {
-	char *filename = (char*)"/dev/i2c-7";  // MCU connected to I2C bus number 7.
-	int file_i2c;
-	int length = 3;  // Number of bytes to write to MCU.
-	int addr = 0x04;  // The I2C address of the MCU.
-	int8_t buffer[3];  // Initialize buffer to write to MCU over I2C.
-	buffer[0] = 0;  // Starting Bit.
-	buffer[1] = theta;  // Theta instruction.
-	buffer[2] = drive;  // Drive instruction.
+	bool DriveAutonomously = false;
+	// bool DriveByUser = false;
+	bool SaveScanData = false;
+	bool Shutdown = false;
+	int8_t joystick_x = 0;
+	int8_t joystick_y = 0;
+}
 
+struct I2C_OUT_PACK
+{
+	virt_joystick_x = 0;
+	virt_joystick_y = 0;
+}
+
+bool
+connect_i2c()
+{
 	if ((file_i2c = open(filename, O_RDWR)) < 0)
 	{
-		printf("Error No. %d", file_i2c);
-		//ERROR HANDLING: you can check errno to see what went wrong.
-		printf("Failed to open the i2c bus");
+		printf("Failed to open the i2c bus. Error No. %d\n", file_i2c);
 		return false;
 	}
-	
+
 	if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
 	{
 		printf("Failed to acquire bus access and/or talk to slave.\n");
 		return false;
 	}
+
+	return true;
+}
+
+bool write_buffer(I2C_OUT_PACK buf_to_write)
+{
+	int length = sizeof(buf_to_write); // Number of bytes to write to MCU.
+	if (!connect_i2c())								 // Connect to I2C bus.
+		return false;
+
 	//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-	if (write(file_i2c, buffer, length) != length)  
+	if (write(file_i2c, buffer, length) != length)
 	{
 		/* ERROR HANDLING: i2c transaction failed */
 		printf("Failed to write to the i2c bus.\n");
@@ -34,4 +53,29 @@ bool write_buffer(int8_t theta, int8_t drive)
 	}
 
 	return true;
+}
+
+I2C_IN_PACK read_buffer()
+{
+	I2C_IN_PACK return_package;
+	int size = sizeof(return_package);
+	if (!connect_i2c()) // Connect to I2C bus.
+		return false;
+
+	if (read(file_i2c, return_package, size) != size)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("I2C: Failure to read all bytes in structure.\n");
+		return false;
+	}
+
+	return return_package;
+}
+
+I2C_IN_PACK rdrw_buffer(I2C_OUT_PACK buf_to_write)
+{
+	write_buffer(buf_to_write);
+	I2C_IN_PACK return_data = read_buffer();
+	if (return_data)
+		return return_data;
 }
--- rplidar/sdk/app/ultra_simple/i2c_comm.h
+++ rplidar/sdk/app/ultra_simple/i2c_comm.h
@@ -1,9 +1,12 @@
-#include <unistd.h>				//Needed for I2C port
-#include <fcntl.h>				//Needed for I2C port
-#include <sys/ioctl.h>			//Needed for I2C port
-#include <linux/i2c-dev.h>		//Needed for I2C port
+#include <unistd.h>        //Needed for I2C port
+#include <fcntl.h>         //Needed for I2C port
+#include <sys/ioctl.h>     //Needed for I2C port
+#include <linux/i2c-dev.h> //Needed for I2C port
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-bool write_buffer(int8_t, int8_t);
+struct I2C_IN_PACK;
+struct I2C_OUT_PACK;
+
+I2C_PACKAGE rdrw_buffer(I2C_PACKAGE);
