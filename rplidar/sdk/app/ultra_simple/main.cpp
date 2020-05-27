33492f289f1494104e2fe2866fe4eb488b8cea2b
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-05-27 21:42:20 +0000

    Added radio control functions.

--- rplidar/sdk/app/ultra_simple/main.cpp
+++ rplidar/sdk/app/ultra_simple/main.cpp
@@ -3,7 +3,7 @@
 #include <signal.h>
 #include <iostream>
 #include <fstream>
-#include "i2c_comm.h"
+#include <i2c_comm.h>
 #include "rplidar.h" //RPLIDAR standard sdk, all-in-one header
 // #include "radio_control.h"  // Arduino is currently handling RC.
 
@@ -19,6 +19,7 @@ u_result op_result;
 bool ctrl_c_pressed;
 bool PREV_AUTO_STATE;
 bool PREV_SAVE_STATE;
+bool LIDAR_ALREADY_SETUP = false;
 I2C_IN_PACK MCU_PACKAGE;
 I2C_OUT_PACK DRIVE_INSTR;
 
@@ -26,11 +27,12 @@ std::ofstream CSV_FILE; // For saving scan data.
 
 struct DataToSave
 {
+	bool invalid_data = false;
 	std::vector<float> theta_deg;
 	std::vector<float> distance_mm;
 	int8_t joystick_x;
 	int8_t joystick_y;
-}
+};
 
 void
 ctrlc(int)
@@ -82,6 +84,9 @@ void setup_lidar()
 	 * Assigns global pointer, `driver`, to an instance of RPLiDAR driver on
 	 * the specified port with baudrate 115200; the baudrate for RPLiDAR A2.
 	 */
+	// if (LIDAR_ALREADY_SETUP)
+	// 	return;
+
 	const char *opt_com_path = NULL;
 	opt_com_path = "/dev/ttyUSB0";
 
@@ -143,6 +148,8 @@ void setup_lidar()
 	std::vector<RplidarScanMode> scanModes;
 	driver->getAllSupportedScanModes(scanModes);
 	driver->startScanExpress(false, scanModes[1].id);
+
+	LIDAR_ALREADY_SETUP = true;
 }
 
 void shutdown_lidar()
@@ -163,7 +170,8 @@ DataToSave get_lidar_data()
 	if (IS_FAIL(op_result))
 	{
 		printf("Failed to get scan data.");
-		return false;
+		tempData.invalid_data = true;
+		return tempData;
 	}
 	else
 	{
@@ -178,23 +186,32 @@ DataToSave get_lidar_data()
 
 int main(int argc, const char *argv[])
 {
+	connect_i2c();
 	signal(SIGINT, ctrlc);
-	MCU_PACKAGE = rdrw_buffer(); // Read/write data from/to arduino.
+	// printf("Auto: " + MCU_PACKAGE.DriveAutonomously);
+	// printf("Save: " + MCU_PACKAGE.SaveScanData);
+	// printf("Shutdown: " + MCU_PACKAGE.Shutdown);
 
+	while (true)
+	{
+	MCU_PACKAGE = rdwr_buffer(DRIVE_INSTR); // Read/write data from/to arduino.
+	// std::cout << "JSX: " << MCU_PACKAGE.joystick_x << "\n";
 	if (MCU_PACKAGE.Shutdown)
 	{
+		printf("Shutdown Initiated.");
 		if (CSV_FILE.is_open())
 		{
 			CSV_FILE << "\nShutdown Initiated While Saving Scan Data";
 			CSV_FILE.close();
 		}
-		system("sudo shutdown now")
+		system("sudo shutdown now");
 	}
 
 	if (MCU_PACKAGE.DriveAutonomously)
 	{
 		if (!PREV_AUTO_STATE) // Need to setup for auto driving.
 		{
+			printf("Autonomous Driving Initiated.\n");
 			PREV_AUTO_STATE = true;
 			setup_lidar();
 		}
@@ -203,33 +220,59 @@ int main(int argc, const char *argv[])
 	}
 	else if (PREV_AUTO_STATE) // Switched autonomous driving off.
 	{
-		PREV_AUTO_STATE = false;
+		printf("Turning Off Autonomous Driving.\n");
 		shutdown_lidar();
+		PREV_AUTO_STATE = false;
 	}
 
 	if (MCU_PACKAGE.SaveScanData)
 	{
+		int scan_counter;
 		if (!PREV_SAVE_STATE) // Setup for saving lidar data.
 		{
-			int scan_counter = 0;
+			printf("Data Save Initiated.");
+			scan_counter = 0;
 			PREV_SAVE_STATE = true;
 			setup_lidar();
 			CSV_FILE.open("SaveScanTest.csv");
 		}
 		DataToSave scan = get_lidar_data();
-		CSV_FILE << "Scan No. " << scan_counter << "\n";
-		CSV_FILE << "Theta, Distance, Joystick X, Joystick Y\n";
-		for (int i = 0; i < lidar_scan.theta_deg.size(); i++)
+
+		CSV_FILE << "Scan, " << scan_counter << "\n";
+		CSV_FILE << "Joystick X, " << (int)MCU_PACKAGE.joystick_x << "\n";
+		CSV_FILE << "Joystick Y, " << (int)MCU_PACKAGE.joystick_y << "\n";
+		CSV_FILE << "Theta, ";  // Following is the writing of each element in theta vector.
+		for (int i = 0; i < scan.theta_deg.size(); i++)
 		{
 			CSV_FILE << scan.theta_deg[i] << ", ";
-			CSV_FILE << scan.distance_mm[i] << ", ";
-			CSV_FILE << MCU_PACKAGE.joystick_x << ", ";
-			CSV_FILE << MCU_PACKAGE.joystick_y << "\n";
 		}
+		CSV_FILE << "\n";  // End line for next entry.
+
+		CSV_FILE << "Distance, ";  // Following is the writing of each element in the distance vector.
+		for (int i = 0; i < scan.distance_mm.size(); i++)
+		{
+			CSV_FILE << scan.distance_mm[i] << ", ";
+		}
+		CSV_FILE << "\n";
+
+		scan_counter += 1;  // Increment `scan_counter` to keep track of data.
 	}
 	else if (PREV_SAVE_STATE)
 	{
+		printf("Turning Off Data Save.\n");
 		CSV_FILE.close();
 		shutdown_lidar();
+		PREV_SAVE_STATE = false;
 	}
+
+	if (ctrl_c_pressed)
+	{
+		printf("Exiting Program.\n");
+		if (CSV_FILE.is_open())
+		{
+			CSV_FILE.close();
+		}
+		return 0;
+	}
+}
 }
