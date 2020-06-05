588a4a59df6ed29d3486c839922b54de8cb33bcd
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-06-05 14:31:45 -0700

    Update load_model.py

--- python/DriverTraining/load_model.py
+++ python/DriverTraining/load_model.py
@@ -5,14 +5,19 @@ from data_manager import process_data, test_train_split
 # load model
 model = load_model('python/DriverTraining/trained_model.h5')
 # summarize model.
-model.summary()
+# model.summary()
 
 # path_to_data = "python/DriverTraining/ScanData/OrigHouseScan1.csv"
 path_to_data = "python/DriverTraining/ScanData/BetterData.csv"
 
 x_data, y_data = process_data(path_to_data)
 x_test, x_train, y_test, y_train = test_train_split(
-    x_data, y_data, test_size=200)
+    x_data, y_data, test_size=1000)
 
-predictions = model.predict(x_test)
-print(predictions * 90)
+results = model.evaluate(x_test, y_test, batch_size=100)
+print(F"Results: {results}\n")
+
+
+# predictions = model.predict(x_test)
+# print(predictions)
+# print(predictions * 90)
