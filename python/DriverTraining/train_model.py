5d90b5df85b7755b167ffa85de423a2a9b0310bd
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-06-07 19:57:07 -0700

    Updated Keras model.

--- python/DriverTraining/train_model.py
+++ python/DriverTraining/train_model.py
@@ -3,15 +3,16 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import time
+import matplotlib.pyplot as plt
 
 from data_manager import process_data, test_train_split
 
 
 def get_uncompiled_model(input_size):
     inputs = keras.Input(shape=input_size, name="scan")
-    x = layers.Conv2D(64, (7, 7), padding="same", activation="relu")(inputs)
+    x = layers.Conv2D(32, (3, 3), padding="same", activation="relu")(inputs)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
-    # x = layers.Conv2D(64, (5, 5), padding="same", activation="relu")(x)
+    # x = layers.Conv2D(32, (5, 5), padding="same", activation="relu")(x)
     # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     # x = layers.Conv2D(128, (5, 5), padding="same", activation="relu")(x)
     # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
@@ -19,13 +20,13 @@ def get_uncompiled_model(input_size):
     # x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     # x = layers.Dense(1024, activation="relu", name="dense_1")(inputs)
     x = layers.Flatten()(x)
-    # x = layers.Dense(512, activation="relu", name="dense_2")(x)
+    # x = layers.Dense(1024, activation="relu", name="dense_2")(x)
     # x = layers.Dense(256, activation="relu", name="dense_3")(x)
     # x = layers.Dense(128, activation="relu", name="dense_4")(x)
-    x = layers.Dense(64, activation="relu", name="dense_5")(x)
+    # x = layers.Dense(64, activation="relu", name="dense_5")(x)
     x = layers.Dense(32, activation="relu", name="dense_6")(x)
     # output_x = layers.Dense(1, activation="linear", name="predictions_x")(x)
-    output_x = layers.Dense(1, activation="tanh", name="predictions_x")(x)
+    output_x = layers.Dense(2, activation="tanh", name="predictions_x")(x)
     model = keras.Model(inputs=inputs, outputs=output_x)
     return model
 
@@ -41,7 +42,9 @@ def get_compiled_model(input_size=(32, 32, 1)):
         # loss=tf.keras.losses.MeanSquaredError(),
         # List of metrics to monitor
         # metrics=[tf.keras.metrics.MeanAbsoluteError()],
-        metrics=[tf.keras.metrics.RootMeanSquaredError()],
+        metrics=[tf.keras.metrics.RootMeanSquaredError(),
+                 tf.keras.metrics.MeanAbsoluteError(),
+                 tf.keras.metrics.MeanSquaredError()],
     )
     return model
 
@@ -53,7 +56,7 @@ if __name__ == "__main__":
 
     x_data, y_data = process_data(path_to_data)
     x_test, x_train, y_test, y_train = test_train_split(
-        x_data, y_data, test_size=3000)
+        x_data, y_data, test_size=1500)
 
     image_shape = x_test.shape[1:]
     print(image_shape)
@@ -65,15 +68,25 @@ if __name__ == "__main__":
     history = model.fit(
         x_train,
         y_train,
-        batch_size=64,
-        epochs=100,
+        batch_size=128,
+        epochs=10,
         verbose=1,
     )
 
     history.history
+    # list all data in history
+    # print(history.history.keys())
+    # # summarize history for loss
+    # plt.plot(history.history['loss'])
+    # # plt.plot(history.history['val_loss'])
+    # plt.title('model loss')
+    # plt.ylabel('loss')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'test'], loc='upper left')
+    # plt.show()
     # Evaluate the model on the test data using `evaluate`
     print("Evaluate on test data")
-    results = model.evaluate(x_test, y_test, batch_size=100)
+    results = model.evaluate(x_test, y_test, batch_size=10)
     print(F"Results: {results}\n")
 
     model.save("python/DriverTraining/trained_model.h5")
